ba25a8fc4c59b9cf24c6b2d67b6483536fb489dd
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-07-16 07:34:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/og4tsk.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/bhuppk.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -43,7 +43,7 @@ OWNER_NAME: process.env.OWNER_NAME || "𝕬𝖛𝖊.𝕭",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© Powered by ① Photo*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/og4tsk.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/bhuppk.jpg",
 // add img for alive msg
 LIVE_MSG: process.env.LIVE_MSG || "> AM ACTIVE *ꀭꀤꈤꅏꂦꂦ*⚡",
 // add alive msg here 
