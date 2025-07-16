6204cd8c72817073871dc97797f5337fd077fb6e
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-07-16 07:31:45 +0200

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/og4tsk.j
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "SHDOW-XTECH",
+BOT_NAME: process.env.BOT_NAME || "ꀭꀤꈤꅏꂦꂦ",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "SHADOW-XTECH",
 // type sticker pack name 
@@ -37,17 +37,17 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "254759000340",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "27767494368",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "Black-Tappy",
+OWNER_NAME: process.env.OWNER_NAME || "𝕬𝖛𝖊.𝕭",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© Powered by Black-Tappy*",
+DESCRIPTION: process.env.DESCRIPTION || "*© Powered by ① Photo*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/og4tsk.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> AM ACTIVE *SHADOW-XTECH*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> AM ACTIVE *ꀭꀤꈤꅏꂦꂦ*⚡",
 // add alive msg here 
-READ_MESSAGE: process.env.READ_MESSAGE || "false",
+READ_MESSAGE: process.env.READ_MESSAGE || "true",
 // Turn true or false for automatic read msgs
 AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
@@ -61,9 +61,9 @@ AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 // make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 // maks true for always online 
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
@@ -71,7 +71,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "254759000340",
+DEV: process.env.DEV || "27767494368",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
